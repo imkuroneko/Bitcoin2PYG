881b866815f0a7057042c5c84ea2ea88facca38e
KuroNeko <kuroinekodev@gmail.com>
2018-03-18 01:36:59 -0300

    :octocat: Upload v2
    Now will twit the Bitcoin price as image

---
+++ bitcoin_price_img.php
@@ -0,0 +1,68 @@
+<?php
+	header	("Content-type: image/png");
+
+	# image size
+	$image	=	ImageCreate(743, 400);
+
+	# image background
+	$image	=	ImageCreateFromPNG('./images/background.png');
+
+	# text colors (rgb)
+	$gray		= ImageColorAllocate($image, 81, 81, 81);
+	$orange		= ImageColorAllocate($image, 237, 128, 33);
+	$light_blue	= ImageColorAllocate($image, 92, 188, 180);
+	$light_gray	= ImageColorAllocate($image, 241, 241, 241);
+
+	# fonts
+	$ubuntu_medium	= './fonts/ubuntu_medium.ttf';
+	$ubuntu_light	= './fonts/ubuntu_light.ttf';
+	$open_sans		= './fonts/open_sans_regular.ttf';
+
+
+	/*======= Bitcoin API requests ========*/
+	require('api_maxicambios.php');
+	require('api_bitcoin.php');
+	/*=====================================*/
+
+	# text
+	$title			= 'Precio del Bitcoin en Guaranies';
+	$coinbase		= number_format(coinbasePYG(), 0, '', '.') .'Gs.';
+	$blockchain		= number_format(MaxiCambios() * BlockChain(), 0, '', '.') .'Gs.';
+	$xapo			= number_format(MaxiCambios() * Xapo(), 0, '', '.') .'Gs.';
+	$bitstamp		= number_format(MaxiCambios() * BitStamp(), 0, '', '.') .'Gs.';
+	$maxicambios	= number_format(MaxiCambios(), 0, '', '.') .'Gs.';
+
+	$footer			= 'Developed by: KuroNeko  /  Maintained by: BitcoinParaguay';
+
+	# load text into image
+	# ($image - font size - angule - coordsX - coordsY - text color - font - text)
+	ImageTTFText ( $image, 26, 0, 35,	75, $orange, $ubuntu_medium, $title );
+	ImageTTFText ( $image, 18, 0, 225, 123, $gray, $open_sans, $coinbase );
+	ImageTTFText ( $image, 18, 0, 225, 159, $gray, $open_sans, $blockchain );
+	ImageTTFText ( $image, 18, 0, 225, 195, $gray, $open_sans, $xapo );
+	ImageTTFText ( $image, 18, 0, 225, 231, $gray, $open_sans, $bitstamp );
+	ImageTTFText ( $image, 18, 0, 225, 306, $light_gray, $open_sans, $maxicambios );
+	ImageTTFText ( $image, 8, 0, 15, 385, $light_blue, $open_sans, $footer );
+
+	# logos
+	$coinbase_logo		= ImageCreateFromPNG('./images/logos/coinbase.png');
+	$blockchain_logo	= ImageCreateFromPNG('./images/logos/blockchain.png');
+	$xapo_logo			= ImageCreateFromPNG('./images/logos/xapo.png');
+	$bitstamp_logo		= ImageCreateFromPNG('./images/logos/bitstamp.png');
+	$maxicambios_logo	= ImageCreateFromPNG('./images/logos/maxicambios.png');
+
+
+	# add logos into our image
+	# ( $image - $image2load - coords_X - coords_Y - source_X_2copy - source_Y_2copy - img_width - size_height )
+	ImageCopy( $image, $coinbase_logo,		45,  90, 0, 0, 150, 50 );
+	ImageCopy( $image, $blockchain_logo,	45, 125, 0, 0, 150, 50 );
+	ImageCopy( $image, $xapo_logo,			45, 160, 0, 0, 150, 50 );
+	ImageCopy( $image, $bitstamp_logo,		45, 195, 0, 0, 150, 50 );
+	ImageCopy( $image, $maxicambios_logo,	57, 280, 0, 0, 127, 36 );
+
+	# render image for show it
+	ImagePNG($image);
+
+	# render for save it
+	ImagePNG($image, './twit.png');
+?>
--- tw1t.php
+++ tw1t.php
@@ -1,20 +1,23 @@
 <?php
-	//	Call 2 Library
+	# Call 2 Library
 	require "twitteroauth/autoload.php";
 	use Abraham\TwitterOAuth\TwitterOAuth;
 
-	//	Our keys 4 Twitter
-	$consumerKey		=	"";
-	$consumerSecret		=	"";
-	$accessToken		=	"";
-	$accessTokenSecret	=	"";
+	# Twitter Credentials
+	define('consumerKey',		'l4SGFeUeNRiMNtnKjZY5hEKVZ');
+	define('consumerSecret',	'O0dG2oxbFJAsXPyTsEv9tUucvsjzGVs3Pm2hC4jaEAV3gJimzH');
+	define('accessToken',		'892179486636224517-Vtf8YpOIjpcgDNIZpAf8eAGVrP7yLpU');
+	define('accessTokenSecret',	'vSCa9djWpakqoAalwNmWIEqhTuE7UwFUfCYcI0Ck0JmVr');
 
-	//	Let's connect to Twitter...
-	$connection			=	new TwitterOAuth( $consumerKey , $consumerSecret , $accessToken , $accessTokenSecret);
 
-	//	Let's get our content for the twit
-	require "bitcoin_price.php";
+	# Open Twitter connection
+	$twitter	=	new TwitterOAuth(consumerKey, consumerSecret, accessToken, accessTokenSecret);
 
-	//	And let's twit it!
-	$statues			=	$connection -> post ( "statuses/update" , array( "status" => $twit_content ));
+	# Let's open our image generator
+	include('bitcoin_price_img.php');
+
+	# And let's twit!
+	$image = $twitter->upload('media/upload', ['media' => './twit.png']);
+	$params = ['media_ids' => implode(',', [$image->media_id_string])];
+	$result = $twitter->post('statuses/update', $params);
 ?>
